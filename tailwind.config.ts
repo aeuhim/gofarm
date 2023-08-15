c94e278e66784a79b908ccd013d17ec525097efd
aeuhim <ericmichaelbedanaconstantino@gmail.com>
2023-08-16 04:41:32 +0800

    Add color taupe

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -6,6 +6,25 @@ const config: Config = {
     './components/**/*.{js,ts,jsx,tsx,mdx}',
     './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
+  theme: {
+    extend: {
+      colors: {
+        taupe: {
+          50: '#fbf9f6',
+          100: '#f3ede4',
+          200: '#e6d8c9',
+          300: '#d9c3af',
+          400: '#ccae94',
+          500: '#8b8275',
+          600: '#7e7367',
+          700: '#675a4e',
+          800: '#50433a',
+          900: '#392c25',
+          950: '#23160f',
+        },
+      },
+    },
+  },
   plugins: [],
 }
 export default config
